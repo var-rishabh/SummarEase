075c5c8902904e17b8adcb565155829126b7f524
Rishabh Varshney <right.rishabh@gmail.com>
2023-12-05 02:32:07 +0530

    Comments Added (v1.4) 💨

--- app.py
+++ app.py
@@ -1,39 +1,49 @@
+# Import necessary modules from Flask
 from flask import Flask, render_template, request
+
+# Import the get_summarized_text function from the summarizer module
 from summarizer import get_summarized_text
 
+# Create a Flask web application
 app = Flask(__name__)
 
 
-def summarizedText(text):
-    return get_summarized_text(text, type=1, lines=10)
+# Define a helper function to generate summarized text
+def summarizedText(text, type=1, lines=10):
+    return get_summarized_text(text, type=type, lines=lines)
 
 
+# Define the main route of the web application
 @app.route("/")
 def main():
     return render_template("main.html")
 
 
+# Define a route for text summarization
 @app.route("/summarize", methods=["GET", "POST"])
 def summarize():
+    # Check if the request method is POST (form submission)
     if request.method == "POST":
+        # Get the input text, summarization type, and lines from the form
         text = request.form.get("text")
         summarizeType = request.form.get("type")
         lines = request.form.get("lines")
+        # Render the summarize HTML template with the summarized text
         return render_template(
             "summarize.html", summary=summarizedText(text, summarizeType, lines)
         )
     else:
+        # If the request method is GET, render the summarize HTML template without summarization
         return render_template("summarize.html")
 
 
+# Define a custom error handler for 404 errors
 @app.errorhandler(404)
 def page_not_found(e):
+    # Render a custom 404.html template when a page is not found
     return render_template("404.html"), 404
 
 
+# Run the Flask web application if the script is executed directly
 if __name__ == "__main__":
-    app.run(
-        host="127.0.0.1",
-        port=8080,
-        debug=True,
-    )
+    app.run()
--- summarizer.py
+++ summarizer.py
@@ -1,18 +1,24 @@
-import extractive as etr
+# Import necessary libraries and modules
 import textblob
 import nltk
 from textblob import TextBlob
 from newspaper import Article
 
+# Assuming this is a custom module for extractive summarization
+import extractive as etr
 
+
+# Function to get a summarized version of the input text
 def get_summarized_text(text, type, lines):
     summary = text
+    # If type is 1, assume the input is a URL and extract the article text
     if type == 1:
         text = extract_article(text)
     summary = etr.generate_summary(text, top_n=lines)
     return summary
 
 
+# Function to extract the text content from a given URL
 def extract_article(url):
     article = Article(url)
     article.download()
